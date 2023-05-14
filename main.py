a078931a889ac395d6e1ed2eff028f697288af9b
Hai Vo <hai.vo@advantest.com>
2023-05-15 01:31:23 +0200

    telebot test

---
+++ main.py
@@ -0,0 +1,26 @@
+import os
+import telebot
+import requests
+
+
+api_key = os.environ['API_KEY']
+username = os.environ['USERNAME']
+password = os.environ['PASSWORD']
+bot = telebot.TeleBot(api_key)
+
+
+def test_request():
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    response = requests.get('https://wetrack.advantest.com/rest/api/2/search?jql=assignee%20%3D%20currentUser()%20AND%20resolution%20%3D%20Unresolved%20order%20by%20updated%20DESC', headers=headers, auth=(username, password))
+    response_dict = response.json()
+    message = ""
+    for issue in response_dict['issues']:
+        issue_key = issue['key']
+        summary = issue['fields']['summary']
+        url = "https://wetrack.advantest.com/browse/{}".format(issue_key)
+        message += f"{issue_key : <10} {summary : ^10} {url : >10}\n\n"
+    return message
+
+bot.send_message('443587718', test_request())
