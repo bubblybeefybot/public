35ba613eac1344f5b1764c06a9f29e6f981385a0
Hai Vo <hai.vo@advantest.com>
2023-05-15 02:05:45 +0200

    correct request call

---
+++ bot.py
@@ -0,0 +1,28 @@
+import os
+import telebot
+import requests
+
+
+api_key = os.environ['TELEGRAM_TOKEN']
+chat_id = os.environ['TELEGRAM_CHAT_ID']
+user = os.environ['WETRACK_USER']
+auth_token = os.environ['WETRACK_AUTH_TOKEN']
+bot = telebot.TeleBot(api_key)
+
+
+def test_request():
+    headers = {
+        'Content-Type': 'application/json',
+    }
+    response = requests.get('https://wetrack.advantest.com/rest/api/2/search?jql=assignee%20%3D%20currentUser()%20AND%20resolution%20%3D%20Unresolved%20order%20by%20updated%20DESC',
+                            headers=headers, auth=(user, auth_token))   
+    response_dict = response.json()
+    message = ""
+    for issue in response_dict['issues']:
+        issue_key = issue['key']
+        summary = issue['fields']['summary']
+        url = "https://wetrack.advantest.com/browse/{}".format(issue_key)
+        message += f"{issue_key : <10} {summary : ^10} {url : >10}\n\n"
+    return message
+
+bot.send_message(chat_id, test_request())
