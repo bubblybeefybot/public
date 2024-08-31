df0d79657a0087f25a81544becb3c42daaec8595
bubblybeefybot <131799589+bubblybeefybot@users.noreply.github.com>
2024-08-31 22:37:26 +0200

    Update bot.py

--- bot.py
+++ bot.py
@@ -17,6 +17,7 @@ def test_request():
     response = requests.get('https://wetrack.advantest.com/rest/api/2/search?jql=assignee%20%3D%20currentUser()%20AND%20resolution%20%3D%20Unresolved%20order%20by%20updated%20DESC',
                             headers=headers)
     response_dict = response.json()
+    print response_dict
     message = "*****Your Open Issues*****\n\n"
     for issue in response_dict['issues']:
         issue_key = issue['key']
